2efea1396c671afcdcc2d3339f35602ceac05c4e
Christopher Weems <hello@christopherweems.com>
2020-11-24 09:49:14 -0500

    Add `Podcast.title(_:)` `.host(_:)`

--- Sources/PodcastGroup/Podcast.swift
+++ Sources/PodcastGroup/Podcast.swift
@@ -8,5 +8,40 @@
 import Foundation
 
 public struct Podcast {
+    public struct Properties {
+        var title: String
+        var hosts = [String]()
+        
+    }
+    
+    internal var properties: Properties
+    
+    fileprivate subscript<V>(propertyKeyPath: WritableKeyPath<Properties, V>) -> V {
+        get { properties[keyPath: propertyKeyPath] }
+        set { properties[keyPath: propertyKeyPath] = newValue }
+    }
+    
+    public init(title: String) {
+        properties = Properties(title: title)
+        
+    }
+    
+}
+
+
+// MARK: - Property Setters
+
+public extension Podcast {
+    func title(_ title: String) -> Self {
+        var new = self
+        new[\.title] = title
+        return new
+    }
+    
+    func host(_ hosts: String...) -> Self {
+        var new = self
+        new[\.hosts].append(contentsOf: hosts)
+        return new
+    }
     
 }
