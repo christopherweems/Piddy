f1093f9f178cc666ba25d31b56748a7597782d09
Christopher Weems <hello@christopherweems.com>
2020-11-24 11:51:28 -0500

    Add `Horror` podcast group

---
+++ Sources/Piddy/Horror.swift
@@ -0,0 +1,36 @@
+//
+//  Horror.swift
+//  
+//
+//  Created by Christopher Weems on 11/24/20.
+//
+
+import PodcastGroup
+
+public struct Horror: PodcastGroup {
+    public var body: Body {
+        "Ghosts In The Burbs"
+        
+        "Knifepoint Horror"
+            .host("Soren Narnia")
+            .url(authority: "knifepointhorror.libsyn.com")
+        
+        "Light House"
+        
+        "Nightmare Magazine"
+        
+        "Nightlight"
+            .host("Tonia Ransom")
+        
+        "The Resurrectionist"
+            .producer("Campfire Radio Theater")
+        
+        "Scare You To Sleep"
+        
+        "Unwell"
+        
+        "Wrong Station"
+        
+    }
+    
+}
