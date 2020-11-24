e20d730c439092b2616703af98c3e14df73f721f
Christopher Weems <hello@christopherweems.com>
2020-11-24 10:03:27 -0500

    Add build expression for podcast argument

--- Sources/PodcastGroup/Protocols/PodcastGroup.swift
+++ Sources/PodcastGroup/Protocols/PodcastGroup.swift
@@ -10,6 +10,14 @@ import MixedGroup
 
 public protocol PodcastGroup {
     typealias Body = MixedGroup<Podcast>
-    var body: Body { get }
+    @MixedGroupBuilder<Podcast> var body: Body { get }
+    
+}
+
+public extension MixedGroupBuilder {
+    static func buildExpression(_ podcastTitle: String) -> MixedGroup<Podcast> {
+        let podcast = Podcast(title: podcastTitle)
+        return .init(elements: [podcast])
+    }
     
 }
