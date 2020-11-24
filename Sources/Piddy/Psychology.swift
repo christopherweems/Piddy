d40d01658bb5440061d4ab6268455b8b7832f7d0
Christopher Weems <hello@christopherweems.com>
2020-11-24 11:36:08 -0500

    Add new podcasts

---
+++ Sources/Piddy/Psychology.swift
@@ -0,0 +1,40 @@
+//
+//  Psychology.swift
+//  
+//
+//  Created by Christopher Weems on 11/24/20.
+//
+
+import PodcastGroup
+
+public struct Psychology: PodcastGroup {
+    public var body: Body {
+        "Hidden Brain"
+            .url("https://www.npr.org/series/423302056/hidden-brain")
+        
+        "On Being"
+            .with(host: "Krista Tippett")
+            .url("https://onbeing.org/series/podcast/")
+        
+        "Popcorn Psychology"
+            .url("https://www.spreaker.com/user/popcorn_psychology")
+        
+        "Speaking of Psychology"
+            .producer("American Psychological Association")
+            .url("https://www.apa.org/research/action/speaking-of-psychology/")
+        
+        "Therapy for Black Girls"
+            .subtitle("The Podcast")
+            .url("https://therapyforblackgirls.com/podcast/")
+        
+        "Unlocking Us"
+            .with(host: "Brené Brown")
+            .url("https://brenebrown.com/unlockingus/")
+        
+        "Where Should We Begin?"
+            .with(host: "Ester Perel")
+            .url("https://www.estherperel.com/podcast")
+        
+    }
+    
+}
--- Sources/Piddy/Technology.swift
+++ Sources/Piddy/Technology.swift
@@ -8,9 +8,21 @@
 import Foundation
 import PodcastGroup
 
-struct Technology: PodcastGroup {
-    var body: Body {
-        fatalError()
+public struct Technology: PodcastGroup {
+    public var body: Body {
+        "Accidental Tech Podcast"
+            .host("Marco Arment", "Casey Liss", "John Siracusa")
+            .url(authority: "atp.fm")
+        
+        "Reply All"
+            .network("Gimlet")
+            .url("https://gimletmedia.com/reply-all/")
+        
+        "Waveform"
+            .subtitle("The MKBHD Podcast")
+            .abbreviation("WVFRM")
+            .host("MKBHD")
+        
     }
     
 }
