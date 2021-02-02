01d5586a951578efd549eb9c7122bb63d6e55911
Christopher Weems <hello@christopherweems>
2021-02-02 17:04:32 -0500

    Update Swift tools version to 5.4

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.4
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -18,9 +18,9 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/gitMcFly/MixedGroup.git", .upToNextMajor(from: "0.0.1")),
+        .package(url: "https://github.com/gitMcFly/MixedGroup.git", .upToNextMajor(from: "0.0.2")),
         .package(url: "https://github.com/apple/swift-algorithms.git", .upToNextMajor(from: "0.0.2")),
-        .package(url: "https://github.com/christopherweems/unstandard.git", .upToNextMajor(from: "0.0.21")),
+        .package(url: "https://github.com/christopherweems/unstandard.git", .upToNextMajor(from: "0.1.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
